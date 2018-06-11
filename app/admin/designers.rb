433808ac43ce36d62e0378ff4561df9c6000f09c
Raghu Betina <raghu@firstdraft.com>
2018-06-11 01:26:37 +0000

    Whitelist columns in Designer for ActiveAdmin

--- app/admin/designers.rb
+++ app/admin/designers.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Designer do
+
+ permit_params :designer_name, :sneaker_id, :designer_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
