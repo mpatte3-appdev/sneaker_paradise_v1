ffb74efe4d5b1ce3bf5e9f2392ace8e9aca44564
Raghu Betina <raghu@firstdraft.com>
2018-06-11 01:26:07 +0000

    Direct association between sneaker_models and designers

--- app/models/designer.rb
+++ app/models/designer.rb
@@ -1,6 +1,9 @@
 class Designer < ApplicationRecord
   # Direct associations
 
+  belongs_to :sneaker,
+             :class_name => "SneakerModel"
+
   # Indirect associations
 
   # Validations
--- app/models/sneaker_model.rb
+++ app/models/sneaker_model.rb
@@ -1,6 +1,9 @@
 class SneakerModel < ApplicationRecord
   # Direct associations
 
+  has_one    :designer,
+             :foreign_key => "sneaker_id"
+
   has_many   :colorways,
              :foreign_key => "sneaker_id",
              :dependent => :nullify
